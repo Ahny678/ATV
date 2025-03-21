6f03b9e7435923ddd666630516c682f4528847e4
Tiffany Nebo <nebotiffany@gmail.com>
2025-03-21 21:48:43 +0100

    Added update, delete, getbyId logic

--- controllers/suscriberController.js
+++ controllers/suscriberController.js
@@ -16,8 +16,49 @@ exports.createSubscriber = async(req, res, next)=>{
         })
         console.log(req.body);
         await newSub.save();
-        res.status(201).json({message:`Subscriber: ${newSub.name} created successfully`})
+        res.status(201).json({message:newSub})
     }catch(err){
         res.status(400).json({message: 'Bad request', error:err})
     }
+}
+
+exports.getASuscriber = async(req,res)=>{
+    try{
+        res.send(res.subscriber.name);
+    }catch(err){
+
+    }
+}
+exports.updateASuscriber = async(req,res)=>{
+    if(req.body.name){
+        res.subscriber.name = req.body.name;
+    }
+    try{
+        const updatedSub = await res.subscriber.save();
+        res.status(200).json({message:`New name: ${updatedSub.name}`})
+    }catch(err){
+        return res.status(500).json({message: 'Error updating subscriber', error:err})
+    }
+}
+exports.deleteASuscriber = async(req,res)=>{
+    try{
+        await res.subscriber.remove();
+        res.status(204).json({message:'Successfully deleted user'})
+
+    }catch(err){
+        return res.status(500).json({message: 'Error deleting subscriber', error:err})
+    }
+}
+
+exports.getSub = async (req, res, next) =>{
+    let subscriber;
+    try{
+        subscriber = await Subscriber.findById(req.params.id);
+        if(subscriber == null) return res.status(404).json({message:'Subscriber does not exits'});
+    }
+    catch(err){
+        return res.status(500).json({message: 'Error retrieving subscriber', error:err})
+    }
+    res.subscriber=subscriber;
+    next();
 }
--- routes/subscribers.js
+++ routes/subscribers.js
@@ -3,5 +3,8 @@ var router = express.Router();
 const subscriberController = require('../controllers/suscriberController')
  
 router.get('/all',subscriberController.getAllSuscribers);
-router.post('/new', subscriberController.createSubscriber)
+router.post('/new', subscriberController.createSubscriber);
+router.get('/:id',subscriberController.getSub, subscriberController.getASuscriber);
+router.patch('/:id',subscriberController.getSub, subscriberController.updateASuscriber);
+router.delete('/:id',subscriberController.getSub, subscriberController.deleteASuscriber);
 module.exports = router;
